bf5d66823f6f867d0530ffdb3adecb7b12a6f1be
tcskrijnen <tim.krijnen9@gmail.com>
2020-05-15 13:30:47 +0200

    Login and save bearer token

--- customerwebsite/src/app/app.config.ts
+++ customerwebsite/src/app/app.config.ts
@@ -1,3 +1,12 @@
 export class AppConfig {
 
+  static ApiBaseURL = 'http://localhost:8762';
+
+  static ApiUrls = {
+    LOGIN: '/login'
+  };
+  static LocalStorageKeys = {
+    TOKEN: 'token',
+  };
+
 }
--- customerwebsite/src/app/login/login.component.ts
+++ customerwebsite/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticationService} from '../REST/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  login(): void {
+    this.authenticationService.getLogin(this.email, this.password);
   }
 
 }
